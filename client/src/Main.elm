904ca2c3f3b3d4104e50b7a2a011047f97937bc3
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-08-19 15:29:14 +0100

    add Elm boilerplate

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,7 +1,69 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, text)
+
+
+
+-- MAIN
 
 
 main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+
+-- MSG
+
+
+type Msg
+    = NoOp
+
+
+
+-- INIT
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( 0, Cmd.none )
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
     text "Hello, Elm!"
