df634dd8279560ee4febfef342ac62242d01a464
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-08-20 17:10:20 +0100

    add variant for answers

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,25 @@ use rocket::fs::FileServer;
 #[derive(Serialize)]
 struct Question {
     statement: String,
-    answers: Vec<String>
+    answers: Vec<Answer>
 }
+
+
+#[derive(Serialize)]
+enum Answer {
+    Right(String),
+    Wrong(String)
+}
+
 
 
 #[get("/api")]
 fn question() -> Json<Question> {
     let statement = String::from("How tall is the Eiffel Tower?");
     let answers = vec![
-        String::from("100m"),
-        String::from("300m"),
-        String::from("900m")
+        Answer::Wrong(String::from("100m")),
+        Answer::Right(String::from("300m")),
+        Answer::Wrong(String::from("900m"))
     ];
     let question = Question { statement, answers };
     Json(question)
