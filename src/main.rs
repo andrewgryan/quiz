6ddf9d917be02e511b06c53b4cbcbd47cdf48cc9
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-08-23 14:12:55 +0100

    use an atomic reference counted mutex containing a hashmap to keep track of the scores

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-
-use std::sync::atomic::{AtomicUsize, Ordering};
-use rocket::State;
-use rocket::serde::{Deserialize, Serialize};
-use rocket::serde::json::Json;
 use rocket::fs::FileServer;
-
-
+use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+use rocket::State;
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
 
 struct Score {
-    count: AtomicUsize
+    count: AtomicUsize,
+    database: Arc<Mutex<HashMap<usize, usize>>>,
 }
 
 #[derive(Serialize)]
 struct Board {
-    score : usize
+    score: usize,
 }
-
 
 #[derive(Serialize)]
 struct Question {
     statement: String,
-    answers: Vec<Answer>
+    answers: Vec<Answer>,
 }
-
 
 #[derive(Serialize, Deserialize, Debug)]
 enum Answer {
     Right(String),
-    Wrong(String)
+    Wrong(String),
 }
-
-
 
 #[get("/api")]
 fn question() -> Json<Question> {
@@ -40,26 +37,44 @@ fn question() -> Json<Question> {
     let answers = vec![
         Answer::Wrong(String::from("100m")),
         Answer::Right(String::from("300m")),
-        Answer::Wrong(String::from("900m"))
+        Answer::Wrong(String::from("900m")),
     ];
     let question = Question { statement, answers };
     Json(question)
 }
 
-
 #[post("/answer", format = "application/json", data = "<data>")]
 fn answer(data: Json<Answer>, score: &State<Score>) -> Json<Board> {
     println!("{:?}", data.into_inner());
 
-    let score = score.count.fetch_add(1, Ordering::Relaxed);
+    let update = score.count.fetch_add(1, Ordering::Relaxed);
 
-    Json(Board { score })
+    // Update atomic reference counted mutex hash map
+    let key = 0;
+    let default = 0;
+    let s: &Score = score.inner();
+    let mut hash_map = s.database.lock().unwrap();
+    let ptr = hash_map.entry(key).or_insert(default);
+    *ptr += 1;
+    println!("mutex: {:?}", hash_map);
+
+    Json(Board { score: update })
 }
 
 #[launch]
 fn rocket() -> _ {
+    // Data structures
+    let hash_map: HashMap<usize, usize> = HashMap::new();
+    let mutex = Arc::new(Mutex::new(hash_map));
+
+    println!("main thread: {:?}", mutex);
+
+    // Rocket server entry point
     rocket::build()
         .mount("/", routes![question, answer])
         .mount("/", FileServer::from("client/dist"))
-        .manage(Score { count: AtomicUsize::new(0) })
+        .manage(Score {
+            count: AtomicUsize::new(0),
+            database: mutex,
+        })
 }
